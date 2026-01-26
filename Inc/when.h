0b47ad6667081fffe9ba9374a8fe85ab02c1bb88
Roy Ratcliffe <roy@ratcliffe.me>
2026-01-26 17:40:11 +0000

    Enhance WHEN_WITH macro for variadic arguments support
    Updated the WHEN_WITH macro to accept variadic arguments,
    allowing for more flexible callback handling in the callback
    framework. This change improves the usability of the macro
    for different scenarios.

--- Inc/when.h
+++ Inc/when.h
@@ -35,10 +35,10 @@
  * \param _when_ The name of the event that triggers the callback.
  * \param _what_ The name of the function to be called when the event occurs.
  */
-#define WHEN_WHAT(_when_, _what_)                                                 \
-    static inline void __##_when_##_with(void *with) { WHEN_WITH(_when_, with); } \
-    static void _what_(void *with);                                               \
-    __attribute__((section(STRINGIFY(when_##_when_)), used)) static void (*const __when__##_what_)(void *with) = &_what_
+#define WHEN_WHAT(_when_, _what_)                                                                                      \
+  static inline void __##_when_##_with(void *with) { WHEN_WITH(_when_, with); }                                        \
+  static void _what_(void *with);                                                                                      \
+  __attribute__((section(STRINGIFY(when_##_when_)), used)) static void (*const __when__##_what_)(void *with) = &_what_
 
 /*!
  * \brief When something happened, do something with it.
@@ -67,27 +67,23 @@
  * causing linker errors.
  */
 #if defined(__TASKING__)
-#define WHEN_WITH(_when_, _with_)                                                                             \
-    do                                                                                                        \
-    {                                                                                                         \
-        extern void (*const _lc_ub_##when_##_when_[])(void *with);                                            \
-        extern void (*const _lc_ue_##when_##_when_[])(void *with);                                            \
-        for (void (*const *what)(void *with) = _lc_ub_##when_##_when_; what < _lc_ue_##when_##_when_; what++) \
-        {                                                                                                     \
-            (**what)(_with_);                                                                                 \
-        }                                                                                                     \
-    } while (0)
+#define WHEN_WITH(_when_, ...)                                                                                         \
+  do {                                                                                                                 \
+    extern void (*const _lc_ub_##when_##_when_[])(void *with, ...);                                                    \
+    extern void (*const _lc_ue_##when_##_when_[])(void *with, ...);                                                    \
+    for (void (*const *what)(void *with, ...) = _lc_ub_##when_##_when_; what < _lc_ue_##when_##_when_; what++) {       \
+      (**what)(__VA_ARGS__);                                                                                           \
+    }                                                                                                                  \
+  } while (0)
 #elif defined(__GNUC__)
-#define WHEN_WITH(_when_, _with_)                                                                              \
-    do                                                                                                         \
-    {                                                                                                          \
-        extern void (*const __start_##when_##_when_[])(void *with) __attribute__((weak));                      \
-        extern void (*const __stop_##when_##_when_[])(void *with) __attribute__((weak));                       \
-        for (void (*const *what)(void *with) = __start_##when_##_when_; what < __stop_##when_##_when_; what++) \
-        {                                                                                                      \
-            (**what)(_with_);                                                                                  \
-        }                                                                                                      \
-    } while (0)
+#define WHEN_WITH(_when_, ...)                                                                                         \
+  do {                                                                                                                 \
+    extern void (*const __start_##when_##_when_[])(void *with, ...) __attribute__((weak));                             \
+    extern void (*const __stop_##when_##_when_[])(void *with, ...) __attribute__((weak));                              \
+    for (void (*const *what)(void *with, ...) = __start_##when_##_when_; what < __stop_##when_##_when_; what++) {      \
+      (**what)(__VA_ARGS__);                                                                                           \
+    }                                                                                                                  \
+  } while (0)
 #endif /* __TASKING__ || __GNUC__ */
 
 #endif /* CANNY_IFX_INC_WHEN_H_ */
