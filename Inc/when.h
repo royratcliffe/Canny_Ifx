7cc039805749a25bd0e968bdb358add005ae4cbb
Roy Ratcliffe <roy@ratcliffe.me>
2026-02-02 11:35:06 +0000

    Refactor WHEN_WITH macro for improved readability
    Enhance the WHEN_WITH macro by adjusting formatting for better
    readability while maintaining functionality across TASKING and
    GNU compilers.

--- Inc/when.h
+++ Inc/when.h
@@ -67,22 +67,22 @@
  * causing linker errors.
  */
 #if defined(__TASKING__)
-#define WHEN_WITH(_when_, ...)                                                                                         \
-  do {                                                                                                                 \
-    extern void (*const _lc_ub_##when_##_when_[])(void *with, ...);                                                    \
-    extern void (*const _lc_ue_##when_##_when_[])(void *with, ...);                                                    \
-    for (void (*const *what)(void *with, ...) = _lc_ub_##when_##_when_; what < _lc_ue_##when_##_when_; what++) {       \
-      (**what)(__VA_ARGS__);                                                                                           \
-    }                                                                                                                  \
+#define WHEN_WITH(_when_, ...)                                                                                                                                 \
+  do {                                                                                                                                                         \
+    extern void (*const _lc_ub_##when_##_when_[])(void *with, ...);                                                                                            \
+    extern void (*const _lc_ue_##when_##_when_[])(void *with, ...);                                                                                            \
+    for (void (*const *what)(void *with, ...) = _lc_ub_##when_##_when_; what < _lc_ue_##when_##_when_; what++) {                                               \
+      (**what)(__VA_ARGS__);                                                                                                                                   \
+    }                                                                                                                                                          \
   } while (0)
 #elif defined(__GNUC__)
-#define WHEN_WITH(_when_, ...)                                                                                         \
-  do {                                                                                                                 \
-    extern void (*const __start_##when_##_when_[])(void *with, ...) __attribute__((weak));                             \
-    extern void (*const __stop_##when_##_when_[])(void *with, ...) __attribute__((weak));                              \
-    for (void (*const *what)(void *with, ...) = __start_##when_##_when_; what < __stop_##when_##_when_; what++) {      \
-      (**what)(__VA_ARGS__);                                                                                           \
-    }                                                                                                                  \
+#define WHEN_WITH(_when_, ...)                                                                                                                                 \
+  do {                                                                                                                                                         \
+    extern void (*const __start_##when_##_when_[])(void *with, ...) __attribute__((weak));                                                                     \
+    extern void (*const __stop_##when_##_when_[])(void *with, ...) __attribute__((weak));                                                                      \
+    for (void (*const *what)(void *with, ...) = __start_##when_##_when_; what < __stop_##when_##_when_; what++) {                                              \
+      (**what)(__VA_ARGS__);                                                                                                                                   \
+    }                                                                                                                                                          \
   } while (0)
 #endif /* __TASKING__ || __GNUC__ */
 
