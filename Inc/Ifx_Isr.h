04762298f25a459aa4f31b782e0b679a1f0fc48c
Roy Ratcliffe <roy@ratcliffe.me>
2026-02-23 18:09:19 +0000

    Refactor IFX_ISR macro to simplify ISR definition
    Removed unnecessary function declaration from the IFX_ISR macro,
    streamlining the way interrupt service routines are defined.

--- Inc/Ifx_Isr.h
+++ Inc/Ifx_Isr.h
@@ -10,8 +10,7 @@
  * \param _pri_ Interrupt priority level.
  * \param ... Additional arguments to be passed to the ISR function.
  */
-#define IFX_ISR(_isr_, _cpu_, _pri_, ...)                          \
-  void cpu##_cpu_##_pri##_pri_##_isr(void) { _isr_(__VA_ARGS__); } \
-  IFX_INTERRUPT(cpu##_cpu_##_pri##_pri_##_isr, _cpu_, _pri_)
+#define IFX_ISR(_isr_, _cpu_, _pri_, ...) \
+  IFX_INTERRUPT(cpu##_cpu_##_pri##_pri_##_isr, _cpu_, _pri_) { _isr_(__VA_ARGS__); }
 
 #endif /* IFX_ISR_H */
